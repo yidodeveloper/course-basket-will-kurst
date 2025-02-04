d0e8ff69041411613d798a5d59311a330c125219
Chori <yidodeveloper@gmail.com>
2025-02-04 15:13:39 +0900

    💡 Create data preprocessing pipeline

---
+++ util/data_preprocessor.py
@@ -0,0 +1,26 @@
+import os
+from pprint import pprint
+import openpyxl
+import json
+
+root_dir = os.getcwd()
+
+excel_file = os.path.join(root_dir, 'util/24-2.xlsx')
+json_file = os.path.join(root_dir, 'app/data/24-2.json')
+
+wb = openpyxl.load_workbook(excel_file)
+sheet = wb.active
+
+headers = ['dept', 'code', 'name', 'prof', 'time', 'allowed_4', 'actual_4', 'allowed_3', 'actual_3', 'allowed_2', 'actual_2', 'allowed_1', 'actual_1']
+
+# Read sheet's data and convert to dictionary
+data = []
+for row in sheet.iter_rows(min_row=2, values_only=True):  # First row is header
+    row_dict = {headers[i]: row[i] for i in range(len(headers))}
+    data.append(row_dict)
+
+pprint(data)
+
+# Save as JSON
+with open(json_file, 'w', encoding='utf-8') as json_file:
+    json.dump(data, json_file, ensure_ascii=False, indent=4)
